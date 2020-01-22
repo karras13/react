a73fa1808538011583283979b661ce98548eddc7
unknown <carrascosajose@hotmail.com>
2020-01-22 09:38:44 +0100

    Initial import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navigation from './Components/navigation';
+import ResponsivePlayer from './Components/video';
+import Main from './Components/main';
+import Footer from './Components/footer';
+
+class App extends Component {
+  render() {
+    return (
+<div>
+      <div>
+        <Navigation className="navbar"/>
+     </div>
+
+      <div>
+        <ResponsivePlayer className="player-wrapper"/>
+     </div>
+
+     <div>
+       <Main className="columns"/>
+     </div>
+
+     <div>
+      <Footer className="page-footer font-small blue"/>
+     </div>
+
+</div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/main.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './MainStyle.css';
+
+class Main extends React.Component {
+  render () {
+    return (
+
+      <div className="container">
+              <div className="row">
+                  <div className="center-block col-12 col-sm-6 col-md-6 col-lg-8 col-xl-3">
+                      <h2>Heading 1</h2>
+
+                      <p className="mt-3">Peter, un ingeniero, tiene pesadillas recurrentes en las que él y todos los que él conoce sufren confrontaciones violentas y alienígenas con un enemigo desconocido. Visita de mala gana una clínica para recibir ayuda psiquiátrica, solo para encontrar un paciente allí que revela que está teniendo las mismas visiones, lo que hace que Peter crea que sus visiones son de una próxima invasión.
+    Esa noche, las naves espaciales invasoras abren fuego sobre la ciudad, causando daños significativos. Peter y su esposa, Alice, barrican su apartamento en medio de los sonidos de matanzas de las tropas terrestres. Un soldado vestido con una armadura rompe la barricada y encuentra a Lucy, una de las hijas de Peter y Alice, escondida debajo de una mesa. El soldado hace una pausa para examinar a la niña, y esa distracción permite que Peter y Alice inmovilicen al soldado. Peter, ahora armado con el arma del soldado, saca a su familia del edificio.
+    Basándose en sus visiones, Peter y su familia acuerdan buscar refugio en la fábrica donde trabaja Peter. Puede pasar por alto la autenticación biométrica del rifle y matar a los soldados que vigilan la salida del edificio de apartamentos. Se dirigen a la entrada de un túnel para viajar con seguridad a la fábrica, pero no antes de que Alice resulte herida por la explosión de una bomba. Cuando se reagrupan, aparece el soldado de su apartamento, habiéndolos rastreado con una señal de homing en el rifle que tomó Peter. Para sorpresa de Peter, el soldado se quita el casco y parece humano. Pedro obliga al soldado a llevar a Alicia a la fábrica. Allí, su jefe David, explica que se ha esperado la invasión durante muchos años. Un médico examina a Alicia pero le informa a Peter que no puede salvarla. Mientras los hombres de David arrastran al soldado invasor para ejecutarlo, le grita a Peter que puede salvar a Alice. Peter acepta quedarse con el soldado para salvar a Alice. David evacuará a sus hijos a una estación de metro donde un tren de transporte los espera para llevarlos a una base fuera del sitio.
+                      </p>
+                  </div>
+
+                  <div className="center-block col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+                    <h2>Heading 1</h2>
+
+                      <p className="mt-3">Peter, un ingeniero, tiene pesadillas recurrentes en las que él y todos los que él conoce sufren confrontaciones violentas y alienígenas con un enemigo desconocido. Visita de mala gana una clínica para recibir ayuda psiquiátrica, solo para encontrar un paciente allí que revela que está teniendo las mismas visiones, lo que hace que Peter crea que sus visiones son de una próxima invasión.
+    Esa noche, las naves espaciales invasoras abren fuego sobre la ciudad, causando daños significativos. Peter y su esposa, Alice, barrican su apartamento en medio de los sonidos de matanzas de las tropas terrestres. Un soldado vestido con una armadura rompe la barricada y encuentra a Lucy, una de las hijas de Peter y Alice, escondida debajo de una mesa. El soldado hace una pausa para examinar a la niña, y esa distracción permite que Peter y Alice inmovilicen al soldado. Peter, ahora armado con el arma del soldado, saca a su familia del edificio.
+    Basándose en sus visiones, Peter y su familia acuerdan buscar refugio en la fábrica donde trabaja Peter. Puede pasar por alto la autenticación biométrica del rifle y matar a los soldados que vigilan la salida del edificio de apartamentos. Se dirigen a la entrada de un túnel para viajar con seguridad a la fábrica, pero no antes de que Alice resulte herida por la explosión de una bomba. Cuando se reagrupan, aparece el soldado de su apartamento, habiéndolos rastreado con una señal de homing en el rifle que tomó Peter. Para sorpresa de Peter, el soldado se quita el casco y parece humano. Pedro obliga al soldado a llevar a Alicia a la fábrica. Allí, su jefe David, explica que se ha esperado la invasión durante muchos años. Un médico examina a Alicia pero le informa a Peter que no puede salvarla. Mientras los hombres de David arrastran al soldado invasor para ejecutarlo, le grita a Peter que puede salvar a Alice. Peter acepta quedarse con el soldado para salvar a Alice. David evacuará a sus hijos a una estación de metro donde un tren de transporte los espera para llevarlos a una base fuera del sitio.
+                  </p>
+
+                  </div>
+
+                  <div className="center-block col-12 col-sm-12 col-md-6 col-lg-8 col-xl-3">
+                    <h2>Heading 1</h2>
+
+                      <p className="mt-3">Peter, un ingeniero, tiene pesadillas recurrentes en las que él y todos los que él conoce sufren confrontaciones violentas y alienígenas con un enemigo desconocido. Visita de mala gana una clínica para recibir ayuda psiquiátrica, solo para encontrar un paciente allí que revela que está teniendo las mismas visiones, lo que hace que Peter crea que sus visiones son de una próxima invasión.
+    Esa noche, las naves espaciales invasoras abren fuego sobre la ciudad, causando daños significativos. Peter y su esposa, Alice, barrican su apartamento en medio de los sonidos de matanzas de las tropas terrestres. Un soldado vestido con una armadura rompe la barricada y encuentra a Lucy, una de las hijas de Peter y Alice, escondida debajo de una mesa. El soldado hace una pausa para examinar a la niña, y esa distracción permite que Peter y Alice inmovilicen al soldado. Peter, ahora armado con el arma del soldado, saca a su familia del edificio.
+    Basándose en sus visiones, Peter y su familia acuerdan buscar refugio en la fábrica donde trabaja Peter. Puede pasar por alto la autenticación biométrica del rifle y matar a los soldados que vigilan la salida del edificio de apartamentos. Se dirigen a la entrada de un túnel para viajar con seguridad a la fábrica, pero no antes de que Alice resulte herida por la explosión de una bomba. Cuando se reagrupan, aparece el soldado de su apartamento, habiéndolos rastreado con una señal de homing en el rifle que tomó Peter. Para sorpresa de Peter, el soldado se quita el casco y parece humano. Pedro obliga al soldado a llevar a Alicia a la fábrica. Allí, su jefe David, explica que se ha esperado la invasión durante muchos años. Un médico examina a Alicia pero le informa a Peter que no puede salvarla. Mientras los hombres de David arrastran al soldado invasor para ejecutarlo, le grita a Peter que puede salvar a Alice. Peter acepta quedarse con el soldado para salvar a Alice. David evacuará a sus hijos a una estación de metro donde un tren de transporte los espera para llevarlos a una base fuera del sitio.
+                    </p>
+
+                  </div>
+
+              </div>
+          </div>
+
+            )
+          }
+        }
+
+  export default Main
